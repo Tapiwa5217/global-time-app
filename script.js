4ca6b20a0488f55eaa2fcfe29860cfec6d8c1ba5
el shamiso <tapiwamusinga1@gmail.com>
2023-04-12 12:58:06 +0200

    edited URL

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ let newOptionClass = document.getElementsByClassName('new-option-class');
 //load timezone options upon page loading
 window.onload = () => {
 
-         fetch('https://cors-everywhere.herokuapp.com/https://worldtimeapi.org/api/timezone')
+         fetch('https://cors-anywhere.herokuapp.com/https://worldtimeapi.org/api/timezone')
          .then((res) => { return res.json(); })
          .then((data) => {   
 
@@ -45,7 +45,7 @@ window.onload = () => {
 
 let newTimeZoneData = () => {
 
-    fetch('https://cors-everywhere.herokuapp.com/https://worldtimeapi.org/api/timezone/'+ timeZoneSelector.value + '')
+    fetch('https://cors-anywhere.herokuapp.com/https://worldtimeapi.org/api/timezone/'+ timeZoneSelector.value + '')
     .then((res) => { return res.json(); })
     .then((data) => {
        
